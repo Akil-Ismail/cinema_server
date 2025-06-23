c52034a79e1615f8ee4b376929389495f06c1430
Akil Ismail <12010232@students.liu.edu.lb>
2025-06-23 21:07:36 +0300

    cinema_server initialization

---
+++ db/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'cinema_db');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME);
